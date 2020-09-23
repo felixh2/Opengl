5544e951a015904def3bcf4c3bfc93f4aa9983e9
donfel <felixh2@gmail.com>
2020-09-23 09:06:16 +0300

    Added new project - rotating cube

--- RotatingCube/main.cpp
+++ RotatingCube/main.cpp
@@ -8,7 +8,6 @@
 #include <glm/glm.hpp>
 #include <GL/freeglut.h>
 
-void display();
 
 #define WIDTH 800
 #define HEIGHT 600
@@ -17,8 +16,6 @@ void display();
 GLint windowWidth = 800;
 GLint windowHeight = 600;
 
-const int num_divisions = 2;
-const int num_vertices = pow(3, num_divisions+1);
 glm::vec2 vertices[NUM_VERTICES];
 
 #define AngleToRad(x)  (3.141/180.0)*x
@@ -118,11 +115,6 @@ void InitializeProgram()
 	std::for_each(shaderList.begin(), shaderList.end(), glDeleteShader);
 }
 
-void copyVertexData()
-{
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-}
-
 void InitializeVertexBuffer()
 {
 	glGenBuffers(1, &vbo);
@@ -130,42 +122,14 @@ void InitializeVertexBuffer()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 }
 
-void triangle(glm::vec2 a, glm::vec2 b, glm::vec2 c)
-{
-	static int index = 0;
-	vertices[index++] = a;
-	vertices[index++] = b;
-	vertices[index++] = c;
-	
-}
-
-void divide_triangle(glm::vec2 a, glm::vec2 b, glm::vec2 c, int k)
-{
-	if (k > 0)
-	{
-		glm::vec2 ab = glm::vec2((a + b)*0.5f);
-		glm::vec2 ac = glm::vec2((a + c)*0.5f);
-		glm::vec2 bc = glm::vec2((b + c)*0.5f);
-
-		divide_triangle(a, ab, ac, k - 1);
-		divide_triangle(b, ab, bc, k - 1);
-		divide_triangle(c, ac, bc, k - 1);
-	}
-	else
-	{
-		triangle(a, b, c);
-	}
-}
-
 void idle()
 {
 	glutPostRedisplay();
 }
 
-static int index = 0;
 void myMouse(int button, int state, int x, int y)
 {
-	
+
 	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
 	{
 		exit(0);
@@ -173,17 +137,10 @@ void myMouse(int button, int state, int x, int y)
 
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
-		glutIdleFunc(NULL);		
-		// we need to convert from the mouse coordinate system (which has the origin on the top left corner) 
-		// to clip coordinate system
-		vertices[index++] = glm::vec2(((float)x / (WIDTH / 2) - 1.0), ((float)(HEIGHT - y) / (HEIGHT / 2) - 1.0));		
+
 	}
 
-	if (index == 3)
-	{
-		index = 0;		
-		glutIdleFunc(idle);
-	}
+
 }
 
 void myKeyboard(unsigned char key, int x, int y)
@@ -194,9 +151,6 @@ void myKeyboard(unsigned char key, int x, int y)
 
 void init()
 {
-	//divide_triangle(glm::vec2(0.0f, 1.0f), glm::vec2(-1.0f, -1.0f), glm::vec2(1.0f, -1.0f), num_divisions);
-	//triangle(glm::vec2(0.0f, 1.0f), glm::vec2(-1.0f, -1.0f), glm::vec2(1.0f, -1.0f));
-
 	InitializeProgram();
 	InitializeVertexBuffer();
 
@@ -215,32 +169,14 @@ void reshape(GLsizei w, GLsizei h)
 	glViewport(0, 0, windowWidth, windowHeight);
 }
 
-void rotateTriangle()
-{
-	float angle =0.1f;
-
-		for (int i = 0; i < 3; i++)
-		{
-			float x = vertices[i].x*cos(AngleToRad(angle)) - vertices[i].y*sin(AngleToRad(angle));
-			float y = vertices[i].x*sin(AngleToRad(angle)) + vertices[i].y*cos(AngleToRad(angle));
-
-			vertices[i].x = x;
-			vertices[i].y = y;
-		}
-	
-	
-
-}
-
 void display()
 {
 
 	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(theProgram);
 
-	rotateTriangle();
+
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	copyVertexData();
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
@@ -248,8 +184,7 @@ void display()
 
 	glDisableVertexAttribArray(0);
 	glUseProgram(0);
-	
-	//glFlush();
+
 	glutSwapBuffers();
 
 }
